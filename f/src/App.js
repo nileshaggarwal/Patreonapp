09b894fbe17111ea5757ae154a25aa57455a8901
Shreyas <shreyxs@gmail.com>
2020-11-28 23:51:24 +0530

    basic signin and signup

--- f/src/App.js
+++ f/src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Signin from "./components/signin/signin";
+import Signup from "./components/signup/signup";
 
-const App = () => <div>hi</div>;
+const App = () => {
+	return (
+		<Router>
+			<Switch>
+				{/* <Route path="/" exact component={Welcome} /> */}
+				<Route path="/signin" exact component={Signin} />
+				<Route path="/signup" exact component={Signup} />
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
---
+++ f/src/components/signin/signin.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+
+class Signin extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			signInEmail: "",
+			signInPassword: "",
+			data: {},
+		};
+	}
+
+	onEmailChange = e => this.setState({ signInEmail: e.target.value });
+	onPasswordChange = e => this.setState({ signInPassword: e.target.value });
+
+	onSubmitSignIn = ev => {
+		ev.preventDefault();
+		fetch(`${process.env.BACKEND}/signin`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				email: this.state.signInEmail,
+				password: this.state.signInPassword,
+			}),
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				if (data.id) this.setState({ data });
+			});
+	};
+	render() {
+		return (
+			<>
+				{this.state.data.name ? (
+					`HI, ${this.state.data.name}!`
+				) : (
+					<>
+						<label htmlFor="email-address">Email</label>
+						<input
+							onChange={this.onEmailChange}
+							type="text"
+							name="email"
+							id="email-address"
+						/>{" "}
+						<br />
+						<label htmlFor="password">Password</label>
+						<input
+							onChange={this.onPasswordChange}
+							type="password"
+							name="password"
+							id="password"
+						/>
+						<br />
+						<input
+							onClick={this.onSubmitSignIn}
+							type="submit"
+							value="Sign in"
+						/>
+					</>
+				)}
+			</>
+		);
+	}
+}
+
+export default Signin;
